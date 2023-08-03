100ded3bec17fff47985ec947e2581f6776a9616
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-08-03 19:12:22 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import { fetchBreeds, fetchCatByBreed } from '../cat-api';
-let array = [];
 const select = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
+const loadingMessage = document.querySelector('.loader');
+const wrongMessage = document.querySelector('.error');
+select.classList.add('hidden');
+wrongMessage.classList.add('hidden');
+let array = [];
+var isResolved = false;
 
 function makeAList(promise) {
   promise
     .then(data => {
+      select.classList.remove('hidden');
+      loadingMessage.classList.add('hidden');
+
       array = data.map(a => {
         return ` <option value=${a.id}>${a.name}</option>`;
       });
@@ -13,24 +21,33 @@ function makeAList(promise) {
     })
     .catch(error => {
       console.log('error while receiving data ' + error);
+      loadingMessage.classList.add('hidden');
+      wrongMessage.classList.remove('hidden');
     });
 }
 
 makeAList(fetchBreeds());
 
 select.addEventListener('change', evnt => {
+  loadingMessage.classList.remove('hidden');
   catInfo.innerHTML = '';
   let string = '';
   let value = evnt.target.value;
 
-  fetchCatByBreed(value).then(a => {
-    string = `<img src="${a[0].url}"width=300px alt="${value}" />
+  fetchCatByBreed(value)
+    .then(a => {
+      loadingMessage.classList.add('hidden');
+      string = `<img src="${a[0].url}"width=300px alt="${value}" />
       <h2>${a[0].breeds[0].name}</h2>
       <p>${a[0].breeds[0].description}</p>
       <p>${a[0].breeds[0].temperament}</p>`;
 
-    catInfo.insertAdjacentHTML('beforeend', string);
-  });
+      catInfo.insertAdjacentHTML('beforeend', string);
+    })
+    .catch(() => {
+      wrongMessage.classList.remove('hidden');
+      loadingMessage.classList.add('hidden');
+    });
 });
 
 // for the text
