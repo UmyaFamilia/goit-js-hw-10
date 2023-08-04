68e6c48354f83db4020829cce43222b6e20b7ade
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-08-04 20:50:29 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-import { fetchBreeds, fetchCatByBreed } from '../cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
 const select = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
-const loadingMessage = document.querySelector('.loader');
+const loadingMessage = document.querySelector('.loder-wrapper');
 const wrongMessage = document.querySelector('.error');
 select.classList.add('hidden');
 wrongMessage.classList.add('hidden');
 let array = [];
-var isResolved = false;
 
+console.log(select);
 function makeAList(promise) {
   promise
     .then(data => {
       select.classList.remove('hidden');
       loadingMessage.classList.add('hidden');
-
       array = data.map(a => {
         return ` <option value=${a.id}>${a.name}</option>`;
       });
       select.insertAdjacentHTML('beforeend', array.join(''));
+      new SlimSelect({
+        select: '#single',
+      });
     })
     .catch(error => {
       console.log('error while receiving data ' + error);
